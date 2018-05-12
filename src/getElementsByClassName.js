daeadcd1f0e89e38b63b16d51547d99a17631273
John Lukenoff <jlukenoff@gmail.com>
2018-05-12 16:05:45 -0700

    solved with recursion

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,24 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  //declare array to store matched elements
+  let elements = [];
+  //searches a list of nodes
+  function searchNodes(nodeList) {
+    //iterate through nodeList...
+    for (let i = 0; i < nodeList.length; i++) {
+      //check if node has target class
+      if (nodeList[i].className.search(className) !== -1) {
+        //push node to elements array
+        elements.push(nodeList[i]);
+      } 
+      //check if node has children
+      if (nodeList[i].children.length !== 0) {
+        searchNodes(nodeList[i].children);
+      }
+    }
+  }
+  searchNodes(document.childNodes[1].children);
+  return elements;
 };
