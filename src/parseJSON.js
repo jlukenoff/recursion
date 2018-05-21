728b5faae36f1050691ec497ac1c94271f686a31
John Lukenoff <jlukenoff@gmail.com>
2018-05-20 18:31:37 -0700

    refactored for concision and style

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -2,174 +2,142 @@
 // var parseJSON = JSON.parse;
 
 // but you're not, so you'll write it from scratch:
-var parseJSON = function(json) {
-
-  //---Helper Functions---//
-
-  
-  //convert strings with quotes and other types into primitive type or plain string
-  function convertToPrimitive(str) {
-    if (typeof str === 'object') return str;
-    str = str.trim();
-    if (str === 'true') {
-      return true;
-    } else if (str === 'false') {
-      return false;
-    } else if (str === 'null') {
-      return null;
-    } else if (str.startsWith('{') || str.startsWith('[')) {
-      return parseJSON(str);
-    } else if (str.startsWith('"') || str.endsWith('"')) {
-      return str.startsWith('"') ? str.slice(1) : str.slice(0, str.length - 1);
-    } else if (!isNaN(str) && str !== '' && str.length !== 5) {
-      return Number(str);
-    } else {
-      return str.trim();
-    }
-  }
-
-  //handles strings with escape characters
-  function escapeHandler(string) {
-    let escapeCounter = 0;
-    for (let i = 0; i < string.length; i++) {
-    }
-  }
-
-
-  //extracts nested json object string when passed a string slice starting at the opening brace
-  function objExtractor(json) {
-    let closeBrace = json[0] === '[' ? ']' : '}';
-    let openBrace = json[0];
-    let braces = 0;
-    for (let i = 1; i < json.length; i++) {
-      let char = json[i]; 
-      if (char === openBrace) { 
-        braces++;
-      } else if (char === closeBrace && braces > 0) {
-        braces--;
-      } else if (char === closeBrace && braces === 0) {
-        let output = json.slice(0, i + 1);
-        return output;
-      }
-    }
-  }
-
-  //returns a string with first and last characted removed
-  function trimEnds(str) {
-    return str.slice(1, str.length - 1);
-  }
-
-  
-  //extracts keys and their respective values into separate arrays from a json string
-  function keyValSort(json) {
-    json = trimEnds(json);
-    let newObj = {
-      keys: [],
-      values: []
-    }
-    let key = true;
-    let inString = false;
-    let str = '';
-    for (let i = 0; i < json.length; i++) {
-      let char = json[i];
-      if (char === ':' && key) {
-        newObj.keys.push(str);
-        str = '';
-        key = false;
-        if (json[i + 1] === ' ') i++;
-
-      } else if (char === '{' || char === '[' && !key) {
-        newObj.values.push(parseJSON(objExtractor(json.slice(i))));
-        i += objExtractor(json.slice(i)).length - 1;
-
-      } else if (char !== '"' && key) {
-        str += char;
-
-      } else if (char === ',' && !key && !inString) {
-        if (str !== '') newObj.values.push(str);
-        str = '';
-        key = true;
-        if (json[i + 1] === ' ') i++;
-
-      } else if (i === json.length - 1) {
-        if (char !== '"') str += char;
-        newObj.values.push(str);
-
-      } else if (char !== '"' && char !== ' ') {
-        str += char;
-
-      } else if (char === '"') {
-        inString ? inString = false : inString = true;
-
-      } else if (char === ' ' && inString) {
-        str += char;
-      }
-    }
-    return newObj;
-  }
-
-  //returns an array from a json array string
-  function parseArray(json) {
-    let inString = false;
-    let newArr = [];
-    let str = '';
-    json = trimEnds(json);
-    for (let i = 0; i < json.length; i++) {
-      let char = json[i];
-      if (char === '\\' && inString) {
-        str += json[i + 1];
-        i++;
-        if (i === json.length - 1) throw(SyntaxError());
-      } else if (char === '"') {
-        inString ? inString = false : inString = true;
-        if (i === json.length - 1 && str.search(/\\/) === -1) {
-          newArr.push(convertToPrimitive(str));
-        } else if (i === json.length - 1) {
-          if (str.endsWith('\\')) throw(SyntaxError());
-          newArr.push(str);
-        }
-
-      } else if (char === '{' || char === '[') {
-        newArr.push(parseJSON(objExtractor(json.slice(i))));
-        i += objExtractor(json.slice(i)).length;
-
-      } else if (char === ',' && !inString) {
-        newArr.push(convertToPrimitive(str));
-        str = '';
-      } else if (inString || char !== ' ') {
-        str += char;
-        if (i === json.length - 1) newArr.push(convertToPrimitive(str));
-      }
-
-    }
-    return newArr;
-  }
-  
-
-  //renders final object from keyValSort output
-  function renderOutputObj(obj) {
-    let newObj = {};
-    obj.keys.forEach(function(key, i) {
-      newObj[convertToPrimitive(key)] = convertToPrimitive(obj.values[i]);
-    });
-    return newObj;
-  }
-  
-
-  //---Control Flow---//
-
+var parseJSON = function (json) {
   if (json.startsWith('[')) {
-    if (!json.endsWith(']')) {
-      throw(SyntaxError());
-    } else {
-      return parseArray(json);
-    }
+    if (!json.endsWith(']')) throw (SyntaxError());
+    return parseArray(json);
   } else if (json.startsWith('{')) {
-    if (json === '{}') return {};
-    let output =  renderOutputObj(keyValSort(json));
-    return output;
+    return renderOutputObj(keyValSort(json));
   }
-};       
+};
+
+//---Helper Functions---//
+
+//extracts keys and their respective values into separate arrays from a json string
+function keyValSort(json) {
+  json = trimEnds(json);
+  let newObj = {
+    keys: [],
+    values: []
+  }
+  let key = true;
+  let inString = false;
+  let str = '';
+  for (let i = 0; i < json.length; i++) {
+    let char = json[i];
+    if (char === ':' && key) {
+      newObj.keys.push(str);
+      str = '';
+      key = false;
+      if (json[i + 1] === ' ') i++;
+    } else if (char === '{' || char === '[' && !key) {
+      newObj.values.push(parseJSON(objExtractor(json.slice(i))));
+      i += objExtractor(json.slice(i)).length - 1;
+    } else if (char !== '"' && key) {
+      str += char;
+    } else if (char === ',' && !key && !inString) {
+      if (str !== '') newObj.values.push(str);
+      str = '';
+      key = true;
+      if (json[i + 1] === ' ') i++;
+    } else if (i === json.length - 1) {
+      if (char !== '"') str += char;
+      newObj.values.push(str);
+    } else if (char !== '"' && char !== ' ') {
+      str += char;
+    } else if (char === '"') {
+      inString ? inString = false : inString = true;
+    } else if (char === ' ' && inString) {
+      str += char;
+    }
+  }
+  return newObj;
+}
+
+//returns an array from a json array string
+function parseArray(json) {
+  let inString = false;
+  let newArr = [];
+  let str = '';
+  json = trimEnds(json);
+  for (let i = 0; i < json.length; i++) {
+    let char = json[i];
+    if (char === '\\' && inString) {
+      str += json[i + 1];
+      i++;
+      if (i === json.length - 1) throw (SyntaxError());
+    } else if (char === '"') {
+      inString ? inString = false : inString = true;
+      if (i === json.length - 1 && str.search(/\\/) === -1) {
+        newArr.push(convertToPrimitive(str));
+      } else if (i === json.length - 1) {
+        if (str.endsWith('\\')) throw (SyntaxError());
+        newArr.push(str);
+      }
+    } else if (char === '{' || char === '[') {
+      newArr.push(parseJSON(objExtractor(json.slice(i))));
+      i += objExtractor(json.slice(i)).length;
+    } else if (char === ',' && !inString) {
+      newArr.push(convertToPrimitive(str));
+      str = '';
+    } else if (inString || char !== ' ') {
+      str += char;
+      if (i === json.length - 1) newArr.push(convertToPrimitive(str));
+    }
+
+  }
+  return newArr;
+}
+
+//extracts nested json object string when passed a string slice starting at the opening brace
+function objExtractor(json) {
+  let closeBrace = json[0] === '[' ? ']' : '}';
+  let openBrace = json[0];
+  let braces = 0;
+  for (let i = 1; i < json.length; i++) {
+    let char = json[i];
+    if (char === openBrace) {
+      braces++;
+    } else if (char === closeBrace && braces > 0) {
+      braces--;
+    } else if (char === closeBrace && braces === 0) {
+      let output = json.slice(0, i + 1);
+      return output;
+    }
+  }
+}
+
+//renders final object from keyValSort output
+function renderOutputObj(obj) {
+  let newObj = {};
+  obj.keys.forEach(function (key, i) {
+    newObj[convertToPrimitive(key)] = convertToPrimitive(obj.values[i]);
+  });
+  return newObj;
+}
+
+//convert strings with quotes and other types into primitive type or plain string
+function convertToPrimitive(str) {
+  if (typeof str === 'object') return str;
+  str = str.trim();
+  if (str === 'true') {
+    return true;
+  } else if (str === 'false') {
+    return false;
+  } else if (str === 'null') {
+    return null;
+  } else if (!isNaN(str) && str !== '' && str.length !== 5) {
+    return Number(str);
+  }
+  return str;
+}
+
+//returns a string with first and last characted removed
+function trimEnds(str) {
+  return str.slice(1, str.length - 1);
+}
+
+
 
 
 
